0c09822b465c55e42ed573dbff8ebc908649c4f7
arturro <arturro@xyz>
2014-07-12 00:35:40 +0200

    powstala baza

--- src/sourceCode/Main.java
+++ src/sourceCode/Main.java
@@ -1,10 +1,4 @@
 package sourceCode;
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.Writer;
 import windows.FakFrame;
 
 public class Main {
@@ -12,21 +6,12 @@ public class Main {
     
     public static void main(String[] args) {
         
+        Batabase database = new Batabase();
+       database.connect();
         
-        WpiszKlienta klient1 = new WpiszKlienta();
         FakFrame frame = new FakFrame();
         frame.setVisible(true);
-        Writer writer = null;
-
-try {
-    writer = new BufferedWriter(new OutputStreamWriter(
-          new FileOutputStream("filename.txt"), "utf-8"));
-    writer.write(klient1.zapiszWPlik());
-} catch (IOException ex) {
-  // report
-} finally {
-   try {writer.close();} catch (Exception ex) {}
-}
+        
     }
     
 }
