ea137d9b792f13b66dc5f96db06c1010eceaa4a1
arturro <arturro@xyz>
2014-07-14 11:13:40 +0200

    cos baza nie dziala

--- src/sourceCode/Batabase.java
+++ src/sourceCode/Batabase.java
@@ -19,41 +19,90 @@ public class Batabase {
       statement = connection.createStatement();
       String customer = "CREATE TABLE IF NOT EXISTS CUSTOMER (ID INT PRIMARY KEY NOT NULL, CUSTOMER TEXT NOT NULL,  ADDRESS TEXT NOT NULL,  TAX_ID TEXT NOT NULL)";
       String product = "CREATE TABLE IF NOT EXISTS PRODUCT (ID INT PRIMARY KEY NOT NULL, INVOICE_ID INT CONSTRAINT invoice_fk REFERENCES invoice (id),PRODUCT TEXT NOT NULL, QUANTITY INT NOT NULL, PRICE INT NOT NULL)";
-      String invoice = "CREATE TABLE IF NOT EXISTS INVOICE (ID INT PRIMARY KEY NOT NULL, KLIENT_ID INT CONSTRAINT klient_fk REFERENCES klient (id),"+
-      "INVOICE_NR TEXT NOT NULL,"+
-      "INVOICE_ADDRESS TEXT NOT NULL,"+
-      "INVOICE_DATE DATETIME NOT NULL,"+
-      "DELIVERY_ADRESS TEXT NOT NULL,"+
-      "CUSTOMER_NUMBER TEXT NOT NULL,"+
-      "SUPPLIER TEXT NOT NULL,"+
-      "TERMS_OF_PAYMENT INT NOT NULL,"+
-      "DATE_OF_PAYMENT DATETIME NOT NULL,"+
-      "TERMS_OF_DELIVERY TEXT NOT NULL,"+
-      "TAX DECIMAL NOT NULL,"+
-      "VALUE DECIMAL NOT NULL,"+
-      "TOTAL DECIMAL NOT NULL,"+
-      "SIGNATURE TEXT NOT NULL,"+
-      "GROSS DECIMAL NOT NULL,"+
-      "NO INT NOT NULL,"+
-      "PRODUCT TEXT NOT NULL,"+
-      "PRICE_PER_UNIT DECIMAL NOT NULL,"+
-      "CURRENCY DECIMAL NOT NULL,"+
-      "QUANTITY INT NOT NULL,"+
-      "UNIT INT NOT NULL,"+
-      "VAT INT NOT NULL)";
+      String invoice = "CREATE TABLE IF NOT EXISTS INVOICE (ID INT PRIMARY KEY NOT NULL, CUSTOMER_ID INT CONSTRAINT customer_fk REFERENCES customer (id),"+
+      "INVOICE_NO TEXT,"+
+      "INVOICE_ADDRESS TEXT,"+
+      "INVOICE_DATE DATETIME,"+
+      "DELIVERY_ADRESS TEXT,"+
+      "CUSTOMER_NO TEXT,"+
+      "SUPPLIER TEXT"+
+      "TERMS_OF_PAYMENT INT,"+
+      "DATE_OF_PAYMENT DATETIME,"+
+      "TERMS_OF_DELIVERY TEXT,"+
+      "TAX DECIMAL,"+
+      "VALUE DECIMAL,"+
+      "TOTAL DECIMAL,"+
+      "SIGNATURE TEXT,"+
+      "GROSS DECIMAL,"+
+      "NO INT,"+
+      "PRODUCT TEXT,"+
+      "PRICE_PER_UNIT DECIMAL,"+
+      "CURRENCY DECIMAL,"+
+      "QUANTITY INT,"+
+      "UNIT INT,"+
+      "VAT INT)";
      
       statement.execute(customer);
       statement.execute(product);
       statement.execute(invoice);
       
       
-    PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO invoice VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
-    //preparedStatement.setString(1, Invoice.);
+    PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO invoice VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
+    preparedStatement.setInt(1,23);
+    preparedStatement.setInt(2,23);
+    preparedStatement.setString(3,"a");
+    preparedStatement.setString(4,"a");
+    preparedStatement.setString(1,"a");
+    preparedStatement.setString(1,"a");
+    preparedStatement.setString(1,"a");
+    preparedStatement.setString(1,"a");
+    preparedStatement.setInt(1,23);
+    preparedStatement.setInt(1,23);
+    preparedStatement.setString(1,"a");
+    preparedStatement.setString(1,"a");
+    preparedStatement.setFloat(1,23);
+    preparedStatement.setFloat(1,23);
+    preparedStatement.setFloat(1,23);
+    preparedStatement.setString(1,"a");
+    preparedStatement.setFloat(1,23);
+    preparedStatement.setInt(1,23);
+    preparedStatement.setString(1,"a");
+    preparedStatement.setFloat(1,23);
+    preparedStatement.setFloat(1,23);
+    preparedStatement.setInt(1,23);
+    preparedStatement.setInt(1,23);
+    preparedStatement.setInt(1,23);
+    
+  
+    
+    
+    
     preparedStatement.execute();
     ResultSet result = statement.executeQuery("SELECT * FROM invoice");
     while (result.next()) {
-    result.getInt("id");
-    result.getString("name");
+    result.getInt("ID");
+    result.getInt ("customerId");
+    result.getString ("invoiceNr");       
+    result.getString ("address");              
+    result.getString ("invoiceDate"); 
+    result.getString ("deliveryAddress");
+    result.getString ("customerNumber");
+    result.getString ("supplier");
+    result.getInt ("termsOfPayment");
+    result.getString ("dueDate"); 
+    result.getString ("termsOfDelivery");  
+    result.getFloat ("tax");
+    result.getFloat ("value"); 
+    result.getFloat ("total");
+    result.getString ("signature");
+    result.getFloat ("gross");
+    result.getInt ("no");
+    result.getString ("product");
+    result.getFloat ("pricePerUnit");
+    result.getFloat ("currency");
+    result.getInt ("quantity");
+    result.getInt ("unit");
+    result.getInt ("vat");
    }
      
     } catch ( Exception exception ) {
