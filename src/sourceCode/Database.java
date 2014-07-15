4b3694792ffa2f7f46af0c6af65b8f8d621f38cd
arturro <arturro@xyz>
2014-07-16 01:24:46 +0200

    java.lang.ArrayIndexOutOfBoundsException: 22, ale juz dziala prep i nie trzeba dawac argumentow do connect

--- src/sourceCode/Database.java
+++ src/sourceCode/Database.java
@@ -2,14 +2,16 @@
 
 package sourceCode;
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.DatabaseMetaData;
 
 public class Database {
     
-  public void connect(int ID, int customerId, String invoiceNo, String address, String invoiceDate,
-          String deliveryAddress, String customerNumber, String supplier,
-          int termsOfPayment, String dueDate, String termsOfDelivery, float tax, 
-          float value, float total, String signature, float gross, int no, float pricePerUnit, 
-          float currency, int quantity, int unit, int vat)
+  public void connect()
   {
     Connection connection = null;
     Statement statement = null;
@@ -21,9 +23,9 @@ public class Database {
       System.out.println("Opened database successfully");
       
       statement = connection.createStatement();
-      String customer = "CREATE TABLE IF NOT EXISTS CUSTOMER (ID INT PRIMARY KEY NOT NULL, CUSTOMER TEXT NOT NULL,  ADDRESS TEXT NOT NULL,  TAX_ID TEXT NOT NULL)";
-      String product = "CREATE TABLE IF NOT EXISTS PRODUCT (ID INT PRIMARY KEY NOT NULL, INVOICE_ID INT CONSTRAINT invoice_fk REFERENCES invoice (id),PRODUCT TEXT NOT NULL, QUANTITY INT NOT NULL, PRICE INT NOT NULL)";
-      String invoice = "CREATE TABLE IF NOT EXISTS INVOICE (ID INT PRIMARY KEY NOT NULL, CUSTOMER_ID INT CONSTRAINT customer_fk REFERENCES customer (id),"+
+      String customerdb = "CREATE TABLE IF NOT EXISTS CUSTOMER (ID INT PRIMARY KEY NOT NULL, CUSTOMER TEXT NOT NULL,  ADDRESS TEXT NOT NULL,  TAX_ID TEXT NOT NULL)";
+      String productdb = "CREATE TABLE IF NOT EXISTS PRODUCT (ID INT PRIMARY KEY NOT NULL, INVOICE_ID INT CONSTRAINT invoice_fk REFERENCES invoice (id),PRODUCT TEXT NOT NULL, QUANTITY INT NOT NULL, PRICE INT NOT NULL)";
+      String invoicedb = "CREATE TABLE IF NOT EXISTS INVOICE (ID INT PRIMARY KEY NOT NULL, CUSTOMER_ID INT CONSTRAINT customer_fk REFERENCES customer (id),"+
       "INVOICE_NO TEXT,"+
       "INVOICE_ADDRESS TEXT,"+
       "INVOICE_DATE DATETIME,"+
@@ -46,39 +48,39 @@ public class Database {
       "UNIT INT,"+
       "VAT INT)";
      
-      statement.execute(customer);
-     statement.execute(product);
-     statement.execute(invoice);
+     statement.execute(customerdb);
+     statement.execute(productdb);
+     statement.execute(invoicedb);
       
       
-    PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO invoice VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
-    preparedStatement.setInt(1,ID);
-    preparedStatement.setInt(2,customerId);
-    preparedStatement.setString(3,invoiceNo);
-    preparedStatement.setString(4,address);
-    preparedStatement.setString(5,invoiceDate);
-    preparedStatement.setString(6,deliveryAddress);
-    preparedStatement.setString(7,customerNumber);
-    preparedStatement.setString(8,supplier);
-    preparedStatement.setInt(9,termsOfPayment); 
-    preparedStatement.setString(10,dueDate);
-    preparedStatement.setString(11,termsOfDelivery);
-    preparedStatement.setFloat(12,tax);
-    preparedStatement.setFloat(13,value);
-    preparedStatement.setFloat(14,total);
-    preparedStatement.setString(15,signature);
-    preparedStatement.setFloat(16,gross);
-    preparedStatement.setInt(17,no);
-    preparedStatement.setString(18,product);
-    preparedStatement.setFloat(19,pricePerUnit);
-    preparedStatement.setFloat(20,currency);
-    preparedStatement.setInt(21,quantity);
-    preparedStatement.setInt(22,unit);
-    preparedStatement.setInt(23,vat);
- 
-    
+    PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO INVOICE VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
+    preparedStatement.setInt(1,1);
+    preparedStatement.setInt(2,2);
+    preparedStatement.setString(3,"invoiceNo");
+    preparedStatement.setString(4,"address");
+    preparedStatement.setString(5,"invoiceDate");
+    preparedStatement.setString(6,"deliveryAddress");
+    preparedStatement.setString(7,"customerNumber");
+    preparedStatement.setString(8,"supplier");
+    preparedStatement.setInt(9,3); 
+    preparedStatement.setString(10,"dueDate");
+    preparedStatement.setString(11,"termsOfDelivery");
+    preparedStatement.setFloat(12,4);
+    preparedStatement.setFloat(13,5);
+    preparedStatement.setFloat(14,6);
+    preparedStatement.setString(15,"signature");
+    preparedStatement.setFloat(16,7);
+    preparedStatement.setInt(17,8);
+    preparedStatement.setString(18,"product");
+    preparedStatement.setFloat(19,9);
+    preparedStatement.setFloat(20,10);
+    preparedStatement.setInt(21,11);
+    preparedStatement.setInt(22,12);
+    preparedStatement.setInt(23,13);
+
     preparedStatement.execute();
-    ResultSet result = statement.executeQuery("SELECT * FROM invoice");
+    
+    ResultSet result = statement.executeQuery("SELECT * FROM INVOICE");
     while (result.next()) {
     result.getInt("ID");
     result.getInt ("customerId");
@@ -103,7 +105,9 @@ public class Database {
     result.getInt ("quantity");
     result.getInt ("unit");
     result.getInt ("vat");
+  System.out.println( "ID = " + 1 );
    }
+         
      
     } catch ( Exception exception ) {
       System.err.println( exception.getClass().getName() + ": " + exception.getMessage() );
@@ -111,6 +115,7 @@ public class Database {
     }
     
     System.out.println("Table created successfully");
+    
   }
 
     
