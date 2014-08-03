b6bc7ed5cb5085e6871d9e5d87181727d9d3e0e4
arturro <arturro@xyz>
2014-08-03 22:19:20 +0200

    zmiana koncepcji, nowe modele i listy

--- src/sourceCode/Main.java
+++ src/sourceCode/Main.java
@@ -1,27 +1,36 @@
 package sourceCode;
+//import java.util.Date;
+
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import javax.swing.JOptionPane;
 import windows.FakFrame;
+import model.Customer;
+import model.Invoicing;
+import java.util.List;
+
 
 public class Main {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         
-      // Invoice invoice = new Invoice();
-       
-       // Database database = new Database();
-       // database.connect();
-        
-        new Database().connect();
-     
-        
-        FakFrame frame = new FakFrame();
-        frame.setVisible(true);
-        
-       
-      System.out.println("Dane faktury: ");
+     // Invoice invoice = new Invoice();
+      Database database = new Database();
+     database.connect();  
+      FakFrame frame = new FakFrame();
+   
       
-      //System.out.println(database. );
+        database.insertCustomer("Warszawa", "Warszawa");
+        database.insertCustomer("Kielce", "Kopenhaga");
+        List<Customer> customer = database.selectCustomer();
+     frame.issueToText.setValue(database.selectCustomer());
+     frame.shipToText.setValue(customer);
+      for(Customer c: customer)
+            System.out.println(c);
+      frame.setVisible(true); 
+             
         
     }
     
