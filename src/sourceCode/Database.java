b80f3688428da4a8b08084709689ffeeb12ba61f
Daniel Iwaniec <d.iwaniec@sville.pl>
2014-09-10 23:25:45 +0200

    database

--- src/sourceCode/Database.java
+++ src/sourceCode/Database.java
@@ -9,15 +9,23 @@ import model.Product;
 import model.Invoicing;
 
 public class Database  {
-    
+
     Connection connection = null;
     Statement statement = null;
-    
-    
+
+    static private Database database = null;
+    static public Database getDatabase() {
+    if (Database.database == null) {
+      Database.database = new Database();
+     }
+
+     return Database.database;
+    }
+
     public void connect()
   {
-    
-   
+
+
     try {
       Class.forName("org.sqlite.JDBC");
       connection = DriverManager.getConnection("jdbc:sqlite:test.db");
@@ -25,68 +33,63 @@ public class Database  {
       System.out.println("Opened database successfully");
       statement = connection.createStatement();
       //nie dzialaja id_customer INTEGER CONSTRAINT customer_fk REFERENCES customer (id)
-      
+
       String createCustomer = "CREATE TABLE IF NOT EXISTS customer (id_customer INTEGER PRIMARY KEY AUTOINCREMENT, customerName varchar(255), companyAddress varchar(255), deliveryAddress varchar(255));";
       String createProduct = "CREATE TABLE IF NOT EXISTS product (id_product INTEGER PRIMARY KEY AUTOINCREMENT,productName varchar(255), price FLOAT);";
       String createInvoicing = "CREATE TABLE IF NOT EXISTS invoicing (id_invoicing INTEGER PRIMARY KEY AUTOINCREMENT, id_customer int, id_product int);";
-  
+
       statement.execute(createCustomer);
-      
-    
-    
       statement.execute(createProduct);
-      
-    
-     
       statement.execute(createInvoicing);
-     this.insertCustomer("Krauze", "Warszawa", "Warszawa");
-     this.insertCustomer("Sołowow","Kielce", "Kopenhaga");
-     this.insertCustomer("Solorz-Żak","Warszawa", "Hawaii");
-     this.insertCustomer("Czarnecki","Londyn", "Pruszków");
-     this.insertProduct("Dąb", 65);
-     this.insertProduct("Brzoza", 55);
-     this.insertProduct("Jesion", 63);
-     this.insertProduct("Czereśnia", 70);
-     
+
+      this.insertCustomer("Krauze", "Warszawa", "Warszawa");
+      this.insertCustomer("Sołowow","Kielce", "Kopenhaga");
+      this.insertCustomer("Solorz-Żak","Warszawa", "Hawaii");
+      this.insertCustomer("Czarnecki","Londyn", "Pruszków");
+      this.insertProduct("Dąb", 65);
+      this.insertProduct("Brzoza", 55);
+      this.insertProduct("Jesion", 63);
+      this.insertProduct("Czereśnia", 70);
+
     } catch ( ClassNotFoundException | SQLException exception ) {
       System.err.println( exception.getClass().getName() + ": " + exception.getMessage() );
       System.exit(0);
     }
-    
+
     System.out.println("Table created successfully");
-   
-  
+
+
   }
 
    public boolean insertCustomer(String customerName, String companyAddress, String deliveryAddress) {
         try {
-    PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO customer VALUES (NULL, ?,?,?);"); 
+    PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO customer VALUES (NULL, ?,?,?);");
     preparedStatement.setString(1,customerName);
     preparedStatement.setString(2,companyAddress);
     preparedStatement.setString(3,deliveryAddress);
-    preparedStatement.execute();             
+    preparedStatement.execute();
         } catch (SQLException e) {
             System.err.println("insertCustomer ERROR");
             return false;
         }
         return true;
-    } 
-   
+    }
+
    public boolean insertProduct(String productName, float price) {
         try {
-    PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO product VALUES (NULL, ?,?);"); 
+    PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO product VALUES (NULL, ?,?);");
     preparedStatement.setString(1,productName);
     preparedStatement.setFloat(2,price);
-    preparedStatement.execute();             
+    preparedStatement.execute();
         } catch (SQLException e) {
             System.err.println("insertProduct ERROR");
             return false;
         }
         return true;
-    } 
+    }
    public boolean insertInvoicing(int idCustomer, int idProduct) {
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO invoicing VALUES (NULL, ?,?);"); 
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO invoicing VALUES (NULL, ?,?);");
             preparedStatement.setInt(1, idCustomer);
             preparedStatement.setInt(2, idProduct);
             preparedStatement.execute();
@@ -96,8 +99,8 @@ public class Database  {
         }
         return true;
     }
-        
-      
+
+
    public List<Customer> selectCustomer() {
         List<Customer> customer = new LinkedList<Customer>();
         try {
@@ -108,24 +111,24 @@ public class Database  {
                 ID = rs.getInt("id_customer");
                 customerName = rs.getString("customerName");
                 companyAddress = rs.getString("companyAddress");
-                deliveryAddress = rs.getString("deliveryAddress");  
-                customer.add(new Customer(ID, customerName,companyAddress, deliveryAddress));  
+                deliveryAddress = rs.getString("deliveryAddress");
+                customer.add(new Customer(ID, customerName,companyAddress, deliveryAddress));
             }
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
         return customer;
-        
+
     }
-   
+
    public List<Product> selectProduct() {
         List<Product> product = new LinkedList<Product>();
         try {
             ResultSet rs = statement.executeQuery("SELECT * FROM product;");
             int ID;
-            String productName;    
-            float price;  
+            String productName;
+            float price;
             while(rs.next()) {
                 ID = rs.getInt("id_product");
                 productName = rs.getString("productName");
@@ -138,43 +141,43 @@ public class Database  {
         }
         return product;
     }
-   
-   public Customer getCustomerById(int ID) throws SQLException{  
-           
+
+   public Customer getCustomerById(int ID) throws SQLException{
+
        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM customer WHERE id_customer = ?;" );
        preparedStatement.setInt(1,ID);
        preparedStatement.executeQuery();
        ResultSet rs = preparedStatement.executeQuery();
-       
+
         Customer customer = new Customer();
          while(rs.next()) {
             customer.setID(rs.getInt(ID));
             customer.setCustomerName(rs.getString("customerName"));
             customer.setCompanyAddress(rs.getString("companyAddress"));
             customer.setDeliveryAddress(rs.getString("deliveryAddress"));
-            
+
          }
         return customer;
-   } 
-   
- public Product getProductById(int ID) throws SQLException{  
-           
+   }
+
+ public Product getProductById(int ID) throws SQLException{
+
        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM product WHERE id_product = ?;" );
        preparedStatement.setInt(1,ID);
        preparedStatement.executeQuery();
        ResultSet rs = preparedStatement.executeQuery();
-       
+
         Product product = new Product();
          while(rs.next()) {
             product.setID(rs.getInt(ID));
             product.setProductName(rs.getString("productName"));
             product.setPrice(rs.getFloat("price"));
-           
-            
+
+
          }
         return product;
-   } 
- 
+   }
+
    public void closeConnection() {
         try {
             connection.close();
--- src/sourceCode/Main.java
+++ src/sourceCode/Main.java
@@ -4,14 +4,14 @@ import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import model.Customer;
 import model.Product;
-import model.Invoicing; 
+import model.Invoicing;
 import java.util.List;
 import windows.FakFrame;
 import windows.SelectItem;
 
 
 public class Main {
-    
+
     static private  FakFrame window = null;
     static public FakFrame getWindow() {
     if (Main.window == null) {
@@ -20,102 +20,102 @@ public class Main {
 
      return Main.window;
     }
-   
-   
+
+
     public static void main(String[] args)  {
-     
-     FakFrame frame = Main.getWindow(); 
+
+     FakFrame frame = Main.getWindow();
      Invoicing invoicing = new Invoicing();
      Database database = new Database();
-     database.connect();  
-    
-     
+     database.connect();
+
+
      Date date = new Date();
      date.Date();
-     
 
-     
-     
+
+
+
      List<Customer> customers = database.selectCustomer();
-     List<Product> products = database.selectProduct();  
+     List<Product> products = database.selectProduct();
 
-  
+
      ///pomocnnicze wyswietlanie
      System.out.println("Customers List: ");
      for(Customer c: customers)
      System.out.println(c);
-     
+
      System.out.println("Products List: ");
      for(Product p: products)
      System.out.println(p);
- 
-     
+
+
          frame.CustomerNoText.removeAllItems();
          for(Customer c: customers){
          frame.CustomerNoText.addItem(new SelectItem(c.getID(),c.getCustomerName()));
      }
-     
-        
+
+
    for (ActionListener listener : frame.CustomerNoText.getActionListeners()) {
    frame.CustomerNoText.removeActionListener(listener);
   }
   frame.CustomerNoText.addActionListener(new java.awt.event.ActionListener() {
    @Override
    public void actionPerformed(java.awt.event.ActionEvent evt) {
-   
-   FakFrame frame = Main.getWindow();  
+
+   FakFrame frame = Main.getWindow();
     Database database = new Database();
-     database.connect();  
+     database.connect();
    Object selectedCustomer = frame.CustomerNoText.getSelectedItem();
    int customerID = ((SelectItem) selectedCustomer).getId();
-  
+
        try {
            Customer customer = database.getCustomerById(customerID);
            frame.issueToText.setValue(customer.getCustomerName()+" - "+ customer.getCompanyAddress());
-           frame.shipToText.setValue(customer.getCustomerName()+" - "+ customer.getDeliveryAddress());        
+           frame.shipToText.setValue(customer.getCustomerName()+" - "+ customer.getDeliveryAddress());
            frame.supplierText.setValue("A&P International");
        } catch (SQLException ex) {
-           
+
        }
-         
+
     }
-  }); 
-          
- 
-  
+  });
+
+
+
   frame.choosingProduct.removeAllItems();
          for(Product p: products){
          frame.choosingProduct.addItem(new SelectItem(p.getID(),p.getProductName()));
      }
-     
-         
+
+
    for (ActionListener listener : frame.choosingProduct.getActionListeners()) {
    frame.choosingProduct.removeActionListener(listener);
   }
   frame.choosingProduct.addActionListener(new java.awt.event.ActionListener() {
    @Override
    public void actionPerformed(java.awt.event.ActionEvent evt) {
-   
-   FakFrame frame = Main.getWindow();  
+
+   FakFrame frame = Main.getWindow();
     Database database = new Database();
-     database.connect();  
+     database.connect();
    Object selectedProduct = frame.choosingProduct.getSelectedItem();
    int productID = ((SelectItem) selectedProduct).getId();
-  
+
      try {
            Product product = database.getProductById(productID);
            frame.listaProduktow.setValueAt(product.getProductName(), 0, 1);
            frame.listaProduktow.setValueAt(product.getPrice(), 0, 2);
        } catch (SQLException ex) {
-           
+
        }
-         
+
     }
-  }); 
-  
- 
-  
+  });
+
+
+
     }
-    
+
   }
-       
+
